bacde92047e803881457a70d6e0a4250aa98f1d9
LucioGhedo <lucioghedina@gmail.com>
2022-07-12 13:55:44 +0200

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <HeaderCompo @search="searchFilm" />
+    </header>
+    <main>
+      <MainCompo :text="userText" />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderCompo from './components/HeaderCompo.vue'
+import MainCompo from './components/MainCompo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderCompo,
+    MainCompo
+  },
+  data() {
+    return {
+      userText: '',
+    }
+  },
+  methods: {
+    searchFilm(text) {
+      this.userText = '';
+      this.userText = text;
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/common.scss';
+
 </style>
---
+++ src/components/HeaderCompo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="back">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div>BOOLFLIX</div>
+                <div class="ms_input">
+                    <input v-model="userSearch" type="text" name="search" id="search">
+                    <button @click="$emit('search', userSearch)" class="btn-primary">Search</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderCompo',
+    data() {
+        return {
+            userSearch: '',
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/common.scss';
+.back {
+    background-color: black;
+}
+div {
+    color: white;
+    font-size: 30px;
+}
+.ms_input {
+    display: flex;
+    padding: 5px 0;
+    input {
+        font-size: 15px;
+    }
+    button {
+        font-size: 15px;
+    }
+}
+</style>
